aa07b7806dd48e7380e5e69ca78c6f0f27507f19
pawel <pawel@webperfect.pl>
2020-02-03 22:33:19 +0100

    Modal, umożliwienie złożenia zamówienia

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Aux from '../../../hoc/Auxiliary'
+import Button from '../../UI/Button/Button'
+
+const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
+        return <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}:</span> {props.ingredients[igKey]}</li>
+    });
+    return (
+        <Aux>
+            <h3>Your order</h3>
+            <p>A delicious burger with the following ingredienst:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Continue to check out?</p>
+            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+        </Aux>
+    )
+};
+
+export default orderSummary;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,8 @@ import React, {Component} from 'react';
 import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+import Modal from '../../components/UI/Modal/Modal'
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -26,7 +28,8 @@ class BurgerBuilder extends Component {
             meat: 0
         },
         totalPrice: 4,
-        purchasable: false
+        purchasable: false,
+        purchasing: false
     }
 
     updatePurchaseState(ingredients) {
@@ -72,6 +75,18 @@ class BurgerBuilder extends Component {
         this.updatePurchaseState(updatedIngredients);
     }
 
+    onPurchase = () => {
+        this.setState({purchasing: true});
+    }
+
+    onPurchaseCancel = () => {
+        this.setState({purchasing: false});
+    }
+
+    onPurchaseContinue = () => {
+        alert('You continue!');
+    }
+
     render() {
 
         // to jest masakra: miałem liczby
@@ -86,12 +101,20 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
+                <Modal show={this.state.purchasing} modalClosed={this.onPurchaseCancel}>
+                <OrderSummary 
+                        ingredients={this.state.ingredients} 
+                        purchaseCancelled={this.onPurchaseCancel} 
+                        purchaseContinued={this.onPurchaseContinue} 
+                        />
+                </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
                     ingredientAdded={this.onAddIngredient}
                     ingredientRemoved={this.onRemoveIngredient}
                     disabled={disabledInfo}
                     purchasable={this.state.purchasable}
+                    ordered={this.onPurchase}
                     price={this.state.totalPrice}
                 />
             </Aux>
