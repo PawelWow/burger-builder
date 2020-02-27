e25b490e0ce9b8d8bc3bcd914ce2716ee66b1ac7
pawel <pawel@webperfect.pl>
2020-02-27 20:12:58 +0100

    saga - dodatkowe opcje

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,4 +1,4 @@
-import { takeEvery } from "redux-saga/effects";
+import { takeEvery, all, takeLatest } from "redux-saga/effects";
 import * as actionTypes from "../actions/actionTypes";
 
 import {
@@ -18,10 +18,16 @@ import {
 } from './order'
 
 export function* watchAuth() {
-    yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
-    yield takeEvery(actionTypes.AUTH_INITITATE_LOGOUT, logoutSaga);
-    yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
-    yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga);
+    yield all([
+        takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga),
+        takeEvery(actionTypes.AUTH_INITITATE_LOGOUT, logoutSaga),
+        takeEvery(actionTypes.AUTH_USER, authUserSaga),
+        takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga)        
+    ]);
+    // yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
+    // yield takeEvery(actionTypes.AUTH_INITITATE_LOGOUT, logoutSaga);
+    // yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
+    // yield takeEvery(actionTypes.AUTH_CHECK_STATE, authCheckStateSaga);
 }
 
 export function* watchBurgerBuilder() {
@@ -29,6 +35,7 @@ export function* watchBurgerBuilder() {
 }
 
 export function* watchOrder(){
-    yield takeEvery(actionTypes.PURCHASE_BURGER_PROCEED, purchaseBurgerSaga);
+    // jakby user napieprzał w button to mozemy dać take latest - weźmie wtedy ostatnie kliknięcie
+    yield takeLatest(actionTypes.PURCHASE_BURGER_PROCEED, purchaseBurgerSaga);
     yield takeEvery(actionTypes.FETCH_ORDERS_PROCEED, fetchOrdersSaga);
 }
